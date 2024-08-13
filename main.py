1b0af808e715798f36bd3e533df810bda38926ed
utkarshminds <pranav.nerurkar@utkarshminds.com>
2024-08-13 17:18:13 +0530

    secrets

--- main.py
+++ main.py
@@ -9,15 +9,54 @@ from services.display_attendance import display_attendance
 from services.insert_dummy_data import insert_dummy_data
 from services.add_attendance import add_attendance
 
+import hmac
+
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+# Main Streamlit app starts here
+
+
 #connect to the database
 
-conn = psycopg2.connect(
-    dbname = 'attendance',
-    user = 'postgres',
-    password = 'postgres',
-    host = 'localhost',
-    port = '5432'
-)
+conn = st.connection("postgresql", type="sql")
 
 cursor = conn.cursor()
 
